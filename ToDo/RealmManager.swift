4a9a3d7e15797e2ba4d764c78d49ce852e8bd447
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-28 23:26:15 +0300

    "Add task" function with realm

--- ToDo/RealmManager.swift
+++ ToDo/RealmManager.swift
@@ -15,6 +15,7 @@ class RealmManager: ObservableObject {
         openRealm()
     }
     
+    // Function to open a Realm - needed for saving data inside of the Realm
     func openRealm() {
         do {
             let config = Realm.Configuration(schemaVersion: 1)
@@ -27,4 +28,27 @@ class RealmManager: ObservableObject {
             print("Error opening Realm: ")
         }
     }
+    
+    // MARK: Add Task Function
+    func addTask(taskTitle: String) {
+        if let localRealm = localRealm { // Need to unwrap optional, since localRealm is optional
+            do {
+                // Trying to write to the localRealm
+                try localRealm.write {
+                    // Creating a new Task
+                    let newTask = Task(value: ["title": taskTitle, "completed": false])
+                   
+                    // Adding newTask to localRealm
+                    localRealm.add(newTask)
+                    
+                    print("Added new task to Realm!", newTask)
+                }
+            } catch {
+                print("Error adding task to Realm: \(error)")
+            }
+        }
+    }
+    
+    
+    
 }
---
+++ ToDo/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  ToDo
+//
+//  Created by Esma Koçak on 28.07.2024.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId // This is our primary key, and each Task instance can be uniquely identified by the ID
+    @Persisted var title = ""
+    @Persisted var completed = false
+}
