f8f24677ef0b534c87eb402e45fe8bf01f9f5103
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-28 23:00:38 +0300

    implementation of Realm Mananger

---
+++ ToDo/RealmManager.swift
@@ -0,0 +1,30 @@
+//
+//  RealmManager.swift
+//  ToDo
+//
+//  Created by Esma Koçak on 28.07.2024.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager: ObservableObject {
+    private(set) var localRealm: Realm?
+    
+    init() {
+        openRealm()
+    }
+    
+    func openRealm() {
+        do {
+            let config = Realm.Configuration(schemaVersion: 1)
+            
+            Realm.Configuration.defaultConfiguration = config
+            
+            localRealm = try Realm()
+            
+        } catch {
+            print("Error opening Realm: ")
+        }
+    }
+}
